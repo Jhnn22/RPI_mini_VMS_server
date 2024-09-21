13d13ba44eb72e6b44dad766d075a5fcb8fb072e
박성민 <sm136599@gmail.com>
2024-09-21 13:53:12 +0900

    feat: create gstthread for gstvideooverlay
    gstreamer 재생을 위해 QThread 상속받은 클래스 생성

---
+++ gstthread.h
@@ -0,0 +1,20 @@
+#ifndef GSTTHREAD_H
+#define GSTTHREAD_H
+
+#include <QWidget>
+#include <QThread>
+
+class GstThread : public QThread
+{
+    Q_OBJECT
+public:
+    GstThread(WId windowId);
+
+protected:
+    void run() override;
+
+private:
+    WId m_windowId;
+};
+
+#endif // GSTTHREAD_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwidget.h"
-
+#include "gstthread.h"
 #include <QApplication>
+
+#include <gst/gst.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    gst_init(&argc, &argv);
     MainWidget w;
     w.show();
+
+    GstThread gstThread(w.winId());
+    gstThread.start();
+
     return a.exec();
 }
